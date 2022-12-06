443a8e0f7476f1742532630ca1530076eadb8b4d
yopifebrian <91509334+yopifebrian@users.noreply.github.com>
2022-12-06 09:45:52 +0700

    update link

--- views/partials/title-meta.php
+++ views/partials/title-meta.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>O-Work</title>
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Fira+Mono">>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Fira+Mono">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="<?php 
   $base_url="http://".$_SERVER['SERVER_NAME'].dirname($_SERVER["REQUEST_URI"].'?').'/';
