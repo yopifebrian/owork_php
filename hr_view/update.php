6af89adc9ac7dc9abd0f9cab3f815402a2603842
yopifebrian <91509334+yopifebrian@users.noreply.github.com>
2022-12-15 08:35:09 +0700

    update yopi

--- hr_view/update.php
+++ hr_view/update.php
@@ -9,9 +9,6 @@ $id_appliance     = $_POST['id_appliance'];
 $tanggal_interview     = $_POST['tanggal_interview'];
 $status           = $_POST['status'];
 
-$query  = "SELECT status FROM appliance WHERE id_appliance='$id_appliance'";
-$result = mysqli_query($conn, $query);
-
 
 
 $sql = "UPDATE appliance 
