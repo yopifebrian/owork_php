0d4b9c642ad5787ba80c49f198948dbf2e62d1ed
march madova jovanovic <marchmadova@telkomuniversity.ac.id>
2022-12-15 08:27:27 +0700

    Create create_campaign.php

---
+++ hr_view/create_campaign.php
@@ -0,0 +1,83 @@
+
+<html>
+<head>
+    <title>Add Campaign</title>
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.js"></script>
+    <style type="text/css">
+        .wrapper{
+            width: 650px;
+            margin: 0 auto;
+        }
+        .page-header h2{
+            margin-top: 0;
+        }
+        table tr td:last-child a{
+            margin-right: 15px;
+        }
+    </style>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+    </script>
+</head>
+ 
+<body>
+<div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+
+
+
+
+
+
+    <a href="index.php">Go to Home</a>
+    <br/><br/>
+ 
+    <form action="add.php" method="post" name="form1">
+        <table width="25%" border="0">
+            <tr> 
+                <td>Judul</td>
+                <td><input type="text" name="judul"></td>
+            </tr>
+            <tr> 
+                <td>Deskripsi</td>
+                <td><input type="text" name="deskripsi"></td>
+            </tr>
+            
+           
+        </table>
+    </form>
+    
+    <?php
+ 
+    // Check If form submitted, insert form data into users table.
+    if(isset($_POST['Submit'])) {
+        $judul = $_POST['judul'];
+        $deskripsi = $_POST['deskripsi'];
+       
+        
+        // include database connection file
+        include_once("config.php");
+                
+        // Insert user data into table
+        $result = mysqli_query($mysqli, "INSERT INTO campaign(title,description) VALUES('$judul','$deskripsi')");
+        
+        // Show message when user added
+        echo "User added successfully. <a href='index.php'>View </a>";
+    }
+    ?>
+
+
+</div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
