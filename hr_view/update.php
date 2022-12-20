094e76187aad216bc13546c8509bbe063a5921f9
yopifebrian <91509334+yopifebrian@users.noreply.github.com>
2022-12-20 11:03:26 +0700

    FINAL

--- hr_view/update.php
+++ hr_view/update.php
@@ -2,15 +2,9 @@
 // Include config file
 require_once "config.php";
  
-
-
-
 $id_appliance     = $_POST['id_appliance'];
 $tanggal_interview     = $_POST['tanggal_interview'];
 $status           = $_POST['status'];
-
-
-
 $sql = "UPDATE appliance 
             SET status='$status', tanggal_interview='$tanggal_interview'
         WHERE id_appliance='$id_appliance'";
